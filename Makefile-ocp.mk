ba673e9dcb73a50822d41a7f6224469e0343aae5
Arda Güçlü <aguclu@redhat.com>
2026-03-06 10:36:46 +0300

    upstream<carry>: Downstream

---
+++ Makefile-ocp.mk
@@ -0,0 +1,12 @@
+include Makefile
+
+.PHONY: verify-ocp
+verify-ocp: manifests
+	@if !(git diff --quiet HEAD); then \
+		git diff; \
+		echo "manifests are out of date, run make manifests"; exit 1; \
+	fi
+
+.PHONY: build-ocp
+build-ocp: fmt vet
+	$(GO_BUILD_ENV) $(GO_CMD) build -ldflags="$(LD_FLAGS)" -o bin/manager cmd/main.go
